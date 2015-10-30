a2764448eb06471af170fd5b68fe888561c0e9f0
Tim Channell <tim.channell@gmail.com>
2015-10-30 14:21:50 -0400

    can manually queue jobs now

--- src/index.js
+++ src/index.js
@@ -15,8 +15,10 @@ var path = require('path');
 function tileReduce(options) {
   var tiles = parseToTiles(options, options.zoom);
   var remaining = tiles.length;
+  var total = remaining;
   var workers = [];
   var workersReady = 0;
+  var autoShutdown = false;
   var ee = new EventEmitter();
   var bar = new ProgressBar(':current / :total tiles (:percent), :elapseds elapsed [:bar] ', {total: remaining});
 
@@ -27,11 +29,13 @@ function tileReduce(options) {
   }
 
   function handleMessage(message) {
-    if (message.ready && ++workersReady === workers.length) run();
-    else if (message.reduce !== undefined) {
+    if (message.ready && ++workersReady === workers.length) {
+      ee.emit('start', tiles);
+      sendTiles(tiles);
+    } else if (message.reduce !== undefined) {
       bar.tick();
       if (message.reduce !== null) ee.emit('reduce', message.reduce);
-      if (--remaining === 0) shutdown();
+      if (--remaining === 0 && autoShutdown) shutdown();
     }
   }
 
@@ -40,11 +44,34 @@ function tileReduce(options) {
     ee.emit('end');
   }
 
-  function run() {
-    ee.emit('start', tiles);
+  function sendTiles(tiles) {
     for (var i = 0; i < tiles.length; i++) {
       workers[i % workers.length].send(tiles[i]);
     }
+  }
+
+  ee.run = function (runoption) {
+    var tileList = parseToTiles(runoption, options.zoom);
+    remaining += tileList.length;
+    total += tileList.length;
+    bar.width = bar.total = total; // reset bar width
+
+    // If workers are all ready, go ahead and queue these up. 
+    if (workersReady === workers.length) {
+      sendTiles(tileList);
+    } else {
+      // Otherwise, we haven't done `sendTiles` yet, so go ahead and add these tiles
+      // to the full list
+      tiles = tiles.concat(tileList);
+    }
+  }
+
+  ee.end = function () {
+    autoShutdown = true;
+
+    // If we finished processing everything and we're sending `end` way after
+    // the fact, autoShutdown won't get triggered, so we end it here.
+    if (remaining === 0 && workersReady === workers.length) shutdown();
   }
 
   return ee;
@@ -59,6 +86,9 @@ function parseToTiles(options, zoom) {
     poly = bboxPolygon(options.bbox);
   } else if (options.geojson) {
     poly = options.geojson;
+  } else {
+    // No job area specified. 
+    return [];
   }
 
   return tilecover.tiles(normalize(poly).features[0].geometry, {min_zoom: zoom, max_zoom: zoom});
